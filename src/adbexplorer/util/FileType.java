ed26ca68ce9af1a8ad131260c5dbbff503f2757a
ahmet0851 <ahmet0851@947a500f-08c2-3a84-87b2-399c25de7a93>
2011-05-23 11:40:03 +0000

    correct some bugs

--- src/adbexplorer/util/FileType.java
+++ src/adbexplorer/util/FileType.java
@@ -29,8 +29,16 @@ public class FileType {
 
 	private String name;
 	private String path;
-	private int type; //0 file, 1 directory, 2 link
+	private int type; //1 file, 2 directory, 3 link, -1 denied file, -2 denied directory, -3 denied link
 	private String permissions;
+	
+	
+	public FileType() {
+		this.name = "";
+		this.path = "";
+		this.type = 0;
+		this.permissions = "";
+	}
 	
 	public FileType(String name, String path, int type, String permissions_s) {
 		super();
@@ -52,7 +60,7 @@ public class FileType {
 		this.path = path;
 	}
 	/**
-	 * @return0 file, 1 directory, 2 link
+	 * @return 1 file, 2 directory, 3 link, -1 denied file, -2 denied directory, -3 denied link
 	 */
 	public int getType() {
 		return type;
@@ -71,13 +79,24 @@ public class FileType {
 		return "ADBObject [name=" + name + ", path=" + path + ", type=" + type + ", permissions=" + permissions + "]";
 	}
 	
+	public int permissionToInteger(String permissions_s) {
+		int retour = 0;
+		try {
+			retour = Integer.parseInt(toNumericRepresentation(permissions_s));
+		}
+		catch (Exception e) {
+			retour = 0;
+		}
+		return retour;
+	}
+	
 	private String toNumericRepresentation(String permissions_s) {
+		System.out.println(permissions_s);
 		String user = toNumericRepresentationHelper(permissions_s.substring(0,3))==0?"0":""+toNumericRepresentationHelper(permissions_s.substring(0,3));
 		String group = toNumericRepresentationHelper(permissions_s.substring(3,6))==0?"0":""+toNumericRepresentationHelper(permissions_s.substring(3,6));
 		String other = toNumericRepresentationHelper(permissions_s.substring(6,9))==0?"0":""+toNumericRepresentationHelper(permissions_s.substring(6,9));
 
 		return user+group+other;
-		
 	}
 	
 	private int toNumericRepresentationHelper(String perm) {
